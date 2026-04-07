85119cac04d40d069b7c718a8f6086215c7ad34c
AAIS Software <aais.software@atomicmail.io>
2026-04-08 00:52:26 +0300

    move controller logic to another file

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import authPlugin from "./auth.js";
 import { PasswordService } from "./auth.js";
 import UserService from "./users/user-service.js";
 import UserRepository from "./users/user-repository.js";
+import buildUserController from "./users/user-controller.js";
 
 const app = Fastify({ logger: true });
 
+// TODO In future - move to DI designated file
 const userRepo = new UserRepository();
 const passwordService = new PasswordService(app);
 const userService = new UserService(app.log, userRepo, passwordService);
@@ -13,43 +15,7 @@ const userService = new UserService(app.log, userRepo, passwordService);
 async function build() {
     await app.register(authPlugin);
 
-    // register
-    app.post("/register", async (req, reply) => {
-        const { username, password } = req.body;
-        const user = await userService.createUserAsync({ username, password });
-
-        if (user.error) {
-            return reply.code(400).send({ error: user.error });
-        }
-
-        return { ok: true };
-    });
-
-    // login
-    app.post("/login", async (req, reply) => {
-        const { username, password } = req.body;
-        const result = await userService.loginUserAsync({ username, password });
-
-        if (result.error) {
-            return reply.code(400).send({ error: result.error });
-        }
-
-        const token = app.jwt.sign({ id: result });
-
-        return { token };
-    });
-
-    // protected
-    app.get("/me", { preHandler: app.authenticate }, async (req, reply) => {
-        const user = await userService.findUserByIdAsync(req.user.id);
-        const response = {
-            id: user.id,
-            username: user.username,
-            created: user.createdTime,
-        };
-
-        return response;
-    });
+    buildUserController(app, userService);
 }
 
 build().then(() => {
---
+++ src/users/user-controller.js
@@ -0,0 +1,39 @@
+export default function buildUserController(app, userService) {
+    // register
+    app.post("/register", async (req, reply) => {
+        const { username, password } = req.body;
+        const user = await userService.createUserAsync({ username, password });
+
+        if (user.error) {
+            return reply.code(400).send({ error: user.error });
+        }
+
+        return { ok: true };
+    });
+
+    // login
+    app.post("/login", async (req, reply) => {
+        const { username, password } = req.body;
+        const result = await userService.loginUserAsync({ username, password });
+
+        if (result.error) {
+            return reply.code(400).send({ error: result.error });
+        }
+
+        const token = app.jwt.sign({ id: result });
+
+        return { token };
+    });
+
+    // protected
+    app.get("/me", { preHandler: app.authenticate }, async (req, reply) => {
+        const user = await userService.findUserByIdAsync(req.user.id);
+        const response = {
+            id: user.id,
+            username: user.username,
+            created: user.createdTime,
+        };
+
+        return response;
+    });
+}
